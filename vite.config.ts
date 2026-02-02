4a348ad6f74a70e5035f1c94cacd53ad85b3331e
HASSAN WARRAICH <hm2257153@gmail.com>
2026-02-02 18:48:34 +0500

    Fix MIME type and postcss configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
   export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/CRUNCHBURGERS/' : '/',
+base: '/CRUNCHBURGERS/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
